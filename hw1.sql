bbd2ffe154039822b2f23d0edb85382f30d8305e
Marco000000000 <102173222+Marco000000000@users.noreply.github.com>
2022-05-20 12:18:57 +0200

    Add files via upload

--- hw1.sql
+++ hw1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 19, 2022 alle 13:39
+-- Creato il: Mag 19, 2022 alle 18:27
 -- Versione del server: 10.4.24-MariaDB
 -- Versione PHP: 7.4.29
 
@@ -41,8 +41,7 @@ CREATE TABLE `carrello` (
 -- Dump dei dati per la tabella `carrello`
 --
 
-INSERT INTO `carrello` (`ID`, `proprietario`, `Totale`, `Nome`, `Descrizione`, `likes`, `commenti`) VALUES
-(1, 'root', 0, NULL, NULL, 0, 0);
+
 
 -- --------------------------------------------------------
 
@@ -125,13 +124,6 @@ CREATE TABLE `prodotti` (
 -- Dump dei dati per la tabella `prodotti`
 --
 
-INSERT INTO `prodotti` (`url`, `Venditore`, `titolo`, `prezzo`, `UrlImg`) VALUES
-('https://www.google.it/aclk?sa=L&ai=DChcSEwiLu5v_rev3AhXrKK0GHSpQBjsYABADGgJwdg&sig=AOD64_306pNBXI0luorGXm0xvWx4dzukvw&ctype=5&q=&ved=0ahUKEwi5yZL_rev3AhXpT98KHfAOCk4Qww8I1gc&adurl=', 'Amazon.it', 'ELEGOO Advanced Starter Kit per progetti per Principianti con Kit di apprendimento Italiano Compatibile con i progetti IDE di Arduino', 45.99, 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTLM9mCvOBoVTXBGqbtc5H6t6XwZXQ_rTadHoowaqAkIF4mzf-DDqY-FCozNrlESmnrkDgtOrQS8UUSY-8VMZQLgUkW-acyrBbOXKVa6P8&usqp=CAE'),
-('https://www.google.it/aclk?sa=l&ai=DChcSEwiLu5v_rev3AhXrKK0GHSpQBjsYABAFGgJwdg&sig=AOD64_08HBKql0LuQT2bjRR9DfMBSztUFA&ctype=5&q=&ved=0ahUKEwi5yZL_rev3AhXpT98KHfAOCk4Qww8I2Qc&adurl=', 'RS Components Italy', 'Arduino Starter Kit versione tedesca multilingue, K040007', 101.76, 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcR2RHcc-El5yLduwS0yRl950BZWPBqw2jbRjzJfAIFABd70qIFVLXcUpJVurqroehBlKIPAqJaA19R_66WWqLO82PKN-ps1BCwFiuwDBuMII9QZzfHs2vyi&usqp=CAE'),
-('https://www.google.it/aclk?sa=l&ai=DChcSEwiLu5v_rev3AhXrKK0GHSpQBjsYABAHGgJwdg&sig=AOD64_0eDoAWfElvi-ruzOoTeQBxcCYpMQ&ctype=5&q=&ved=0ahUKEwi5yZL_rev3AhXpT98KHfAOCk4Qww8I3Ac&adurl=', 'Arduino Official Store', 'Arduino Starter Kit [Italiano   Manuale Ufficiale]', 79.9, 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRHuMT3wMqwEahxCG_M2Jv9hiR_bj1C5fUbpxmeanD9O9lUPeKYwTQmsiW6DrNd0qTWtSQzCP554t1SsdGETsJt3m3LKER8OE8xQokG25m9ahZ9icW9AJJjGw&usqp=CAE'),
-('https://www.google.it/aclk?sa=L&ai=DChcSEwiLu5v_rev3AhXrKK0GHSpQBjsYABAJGgJwdg&sig=AOD64_1qlDFx9ZAXfL-nOUPxhvt1xQoFlg&ctype=5&q=&ved=0ahUKEwi5yZL_rev3AhXpT98KHfAOCk4Qww8I3wc&adurl=', 'Amazon.it', 'ELEGOO MEGA R3 Lo Starter Kit più Completo con Tutorial Italiano Compatibile con i Progetti IDE di Arduino', 73.99, 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRahb2UxFo_CpIcdprjdL18W_OC5842q_ycqXQ_vdKy4oQDx98Vi0bGxwL9WCjhZnBQevVV2jVxOgUWFtWYZvzG-qimp6gXwVF938cHO_0&usqp=CAE'),
-('https://www.google.it/aclk?sa=l&ai=DChcSEwiLu5v_rev3AhXrKK0GHSpQBjsYABCDARoCcHY&sig=AOD64_3iBYlT8ybOs4C-WAACzdzS1l4_Ug&ctype=5&q=&ved=0ahUKEwi5yZL_rev3AhXpT98KHfAOCk4Q9A4IvQ0&adurl=', 'RS Components Italy', 'Arduino Uno Rev 3, A000066', 24.8, 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTSyoRMuv9DazhJf7W7cFDluAqWfzzOkeYqMQtyoUmQlaiG-uTH3bykWU8qu47mEO5yJJwv12o7DJxJ_bWaxM4VsXLnfmrgb8uOI-inIVtGeU81G46EPP9h5Q&usqp=CAE'),
-('https://www.google.it/aclk?sa=L&ai=DChcSEwiLu5v_rev3AhXrKK0GHSpQBjsYABCJARoCcHY&sig=AOD64_0OAIrLDsE90cQTDpxzfNhQG54kvA&ctype=5&q=&ved=0ahUKEwi5yZL_rev3AhXpT98KHfAOCk4Q9A4Ixg0&adurl=', 'Amazon.it', 'Smraza Progetto Starter Kit con Breadboard Holder, DC Motore per Principianti con Manuale Compatibile con Arduino Mega2560 Mega328 Nano', 28.99, 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTe_gTd-waSiGSvrzGfpymFDGzk2IN2Ou2BmS4Ed1QukZpCzRVywG1KDFzhGAsKhGgMgKdrZAySCF8Io7pc0AM_SYFQ6K3o9JqOZoZhLkrlGPU1r89rSfv8Cg&usqp=CAE');
 
 -- --------------------------------------------------------
 
@@ -141,18 +133,14 @@ INSERT INTO `prodotti` (`url`, `Venditore`, `titolo`, `prezzo`, `UrlImg`) VALUES
 
 CREATE TABLE `prodotto-carrello` (
   `prodotto` varchar(255) NOT NULL,
-  `carrello` int(11) NOT NULL
+  `carrello` int(11) NOT NULL,
+  `quantita` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dump dei dati per la tabella `prodotto-carrello`
 --
 
-INSERT INTO `prodotto-carrello` (`prodotto`, `carrello`) VALUES
-('https://www.google.it/aclk?sa=l&ai=DChcSEwiLu5v_rev3AhXrKK0GHSpQBjsYABAFGgJwdg&sig=AOD64_08HBKql0LuQT2bjRR9DfMBSztUFA&ctype=5&q=&ved=0ahUKEwi5yZL_rev3AhXpT98KHfAOCk4Qww8I2Qc&adurl=', 1),
-('https://www.google.it/aclk?sa=l&ai=DChcSEwiLu5v_rev3AhXrKK0GHSpQBjsYABAHGgJwdg&sig=AOD64_0eDoAWfElvi-ruzOoTeQBxcCYpMQ&ctype=5&q=&ved=0ahUKEwi5yZL_rev3AhXpT98KHfAOCk4Qww8I3Ac&adurl=', 1),
-('https://www.google.it/aclk?sa=L&ai=DChcSEwiLu5v_rev3AhXrKK0GHSpQBjsYABAJGgJwdg&sig=AOD64_1qlDFx9ZAXfL-nOUPxhvt1xQoFlg&ctype=5&q=&ved=0ahUKEwi5yZL_rev3AhXpT98KHfAOCk4Qww8I3wc&adurl=', 1),
-('https://www.google.it/aclk?sa=l&ai=DChcSEwiLu5v_rev3AhXrKK0GHSpQBjsYABCDARoCcHY&sig=AOD64_3iBYlT8ybOs4C-WAACzdzS1l4_Ug&ctype=5&q=&ved=0ahUKEwi5yZL_rev3AhXpT98KHfAOCk4Q9A4IvQ0&adurl=', 1);
 
 -- --------------------------------------------------------
 
@@ -173,8 +161,6 @@ CREATE TABLE `profilo` (
 -- Dump dei dati per la tabella `profilo`
 --
 
-INSERT INTO `profilo` (`Username`, `Password`, `Email`, `ImmagineProfilo`, `carrelloCorrente`, `seguaci`) VALUES
-('root', 'admin1', 'root@root.it', NULL, 1, 0);
 
 --
 -- Trigger `profilo`
@@ -191,10 +177,10 @@ DELIMITER ;
 DELIMITER $$
 CREATE TRIGGER `numeroCarrello` BEFORE INSERT ON `profilo` FOR EACH ROW BEGIN
 
-if (SELECT (max(id)+1) FROM `carrello`) != null THEN
+if ((SELECT max(id) FROM `carrello`) >0) THEN
 	SET new.carrelloCorrente=(SELECT (max(id)+1) FROM `carrello`);
 ELSE
-	set new.carrelloCorrente=1;
+	set new.carrelloCorrente=2;
 end IF;
 
 
@@ -291,7 +277,7 @@ ALTER TABLE `segui`
 -- AUTO_INCREMENT per la tabella `carrello`
 --
 ALTER TABLE `carrello`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Limiti per le tabelle scaricate
